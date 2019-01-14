1cb894661035ac4b3a6b2a5b0eb422989bd5101b
Mike Wampler <mikewampler@gmail.com>
2019-01-14 14:29:48 -0500

    2nd

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:passwordbab@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2:passwordbab@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -18,21 +18,77 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1024))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    #if request.method == 'POST':
+    #    blog_name = request.form['title']
+    #    blog_body = request.form['body']
+    #    new_blog = Blog(blog_name, blog_body)
+    #    db.session.add(new_blog)
+    #    db.session.commit()
+
+    
+    blogs = Blog.query.all()
+    #completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('blog.html',title="Blogs", 
+        blogs=blogs) #, completed_tasks=completed_tasks)
+
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_blog():
+
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+        blog_name = request.form['title']
+        blog_body = request.form['body']
+        #new_blog = Blog(blog_name, blog_body)
+        #db.session.add(new_blog)
+        #db.session.commit()
+
+        rtn_error1 = ""
+        rtn_error2 = ""
+        error = False
+        if (len(blog_name) < 1): 
+            rtn_error1 = "Please fill in the title"
+            error = True
+        if (len(blog_body) <1):
+            rtn_error2 = "Please fill in the body"
+            error = True   
+        if error:
+            return render_template('newpost.html', title=blog_name, body=blog_body, 
+                error1=rtn_error1, error2=rtn_error2) # and cgi.escape(encoded_error, quote=True))
+
+        # Add the new blog to the database
+        new_blog = Blog(blog_name, blog_body)
+        db.session.add(new_blog)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
+    blogs = Blog.query.all()
+    #completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('blog.html',title="Blogs", 
+        blogs=blogs) #, completed_tasks=completed_tasks)
+    #return redirect('/')
 
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+
+    #task_id = int(request.form['task-id'])
+    #task = Task.query.get(task_id)
+    #task.completed = True
+    #db.session.add(task)
+    #db.session.commit()
+
+    return redirect('/')
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
